9c86bb8311531bf21b609984c48918c4fc3a4f01
Marek Karzel <karzel.marek@udo.edu>
2018-12-08 18:51:56 +0100

    Horrortabelle

--- V.101/latex-template/content/traegheit.py
+++ V.101/latex-template/content/traegheit.py
@@ -8,6 +8,7 @@ from uncertainties import ufloat
 r = ufloat(0.032, 0.0005) / 2
 Vk1 = (4 / 3) * np.pi * r **3
 rk1 = r
+#print(rk1, Vk1)
 
 #Kegelstumpf 
 r1 = ufloat(0.032, 0.0005) / 2
@@ -16,11 +17,13 @@ h = ufloat(0.045, 0.0005)
 Vk2 = (h * np.pi / 3) * (r1**2 + r1 * r2 + r2**2)
 rk21 = r1
 rk22 = r2
+#print(r1, r2, h, Vk2)
 
 #Halskugel
 r = ufloat(0.016, 0.0005) / 2 
 Vk3 = (4 / 3) * np.pi * r**3
 rk3 = r
+#print(r, Vk3)
 
 #Torso
 
@@ -32,6 +35,7 @@ Vt1 = a * b * c
 at1 = a
 bt1 = b
 ct1 = c
+#print(a, b, c, Vt1)
 
 #Zylinder
 r = ufloat(0.0257, 0.0005) / 2
@@ -39,6 +43,7 @@ h = ufloat(0.0133, 0.0005)
 Vt2 = h * np.pi * r**2
 rt2 = r
 ht2 = h
+#print(r, h, Vt2)
 
 #Kegelstumpf
 r1 = ufloat(0.0303, 0.0005) / 2
@@ -48,6 +53,7 @@ Vt3 = (h * np.pi / 3) * (r1**2 + r1 * r2 + r2**2)
 rt31 = r1
 rt32 = r2
 ht3 = h
+#print(r1, r2, h, Vt3)
 
 #Arme
 
@@ -55,6 +61,7 @@ ht3 = h
 r = ufloat(0.016, 0.0005) / 2 
 Va1 = (4 / 3) * np.pi * r**3
 ra1 = r
+#print(r, Va1)
 
 #Bizepszylinder
 r = ufloat(0.016, 0.0005) / 2
@@ -62,11 +69,13 @@ h = ufloat(0.0428, 0.0005)
 Va2 = h * np.pi * r**2
 ra2 = r
 ha2 = h
+#print(r, h, Va2)
 
 #Ellbogenkugel
 r = ufloat(0.0116, 0.0005) / 2 
 Va3 = (4 / 3) * np.pi * r**3
 ra3 = r
+#print(r, Va3)
 
 #Unterarmzylinder
 r = ufloat(0.0143, 0.0005) / 2
@@ -74,18 +83,21 @@ h = ufloat(0.0428, 0.0005)
 Va4 = h * np.pi * r**2
 ra4 = r
 ha4 = h
+#print(r, h, Va4)
 
 #Handkugel
 r = ufloat(0.0095, 0.0005) / 2 
 Va5 = (4 / 3) * np.pi * r**3
 ra5 = r
+#print(r, Va5)
 
-#Halbkegel
-r = ufloat(0.0155, 0.0005)
+#Halbzylinder
+r = ufloat(0.0155, 0.0005) / 2
 h = ufloat(0.0267, 0.0005)
-Va6 = (1 / 6) * np.pi * r**2 * h
+Va6 = np.pi * r**2 * h
 ra6 = r
 ha6 = h
+#print(r, h, Va6)
 
 #Beine
 
@@ -93,6 +105,7 @@ ha6 = h
 r = ufloat(0.0168, 0.0005) / 2 
 Vb1 = (4 / 3) * np.pi * r**3
 rb1 = r
+#print(r, Vb1)
 
 #Oberschenkelzylinder
 r = ufloat(0.0182, 0.0005) / 2
@@ -100,11 +113,13 @@ h = ufloat(0.0564, 0.0005)
 Vb2 = h * np.pi * r**2
 rb2 = r
 hb2 = h
+#print(r, h, Vb2)
 
 #Kniekugel
 r = ufloat(0.0125, 0.0005) / 2 
 Vb3 = (4 / 3) * np.pi * r**3
 rb3 = r
+#print(r, Vb3)
 
 #Unterschenkelzylinder
 r = ufloat(0.0147, 0.0005) / 2
@@ -112,18 +127,23 @@ h = ufloat(0.0663, 0.0005)
 Vb4 = h * np.pi * r**2
 rb4 = r
 hb4 = h
+#print(r, h, Vb4)
 
 #Fußgelenkkugel
 r = ufloat(0.0092, 0.0005) / 2 
 Vb5 = (4 / 3) * np.pi * r**3
 rb5 = r
+#print(r, Vb5)
 
-#Fußzylinder
-r = ufloat(0.0108, 0.0005) / 2
-h = ufloat(0.0423, 0.0005)
-Vb6 = (1 / 2) * h * np.pi * r**2
-rb6 = r
-hb6 = h
+#Fußquader
+c = ufloat(0.0108, 0.0005)
+a = c / 2
+b = ufloat(0.0423, 0.0005)
+Vb6 = a * b * c
+ab6 = a
+bb6 = b
+cb6 = c
+print(a, b, c, Vb6)
 
 #Gesamtvolumen
 V = Vk1 + Vk2 + Vk3 + Vt1 + Vt2 + Vt3 + 2 * (Va1 + Va2 + Va3 + Va4 + Va5 + Va6) + 2 * (Vb1 + Vb2 + Vb3 + Vb4 + Vb5 + Vb6)
@@ -166,7 +186,7 @@ Ib2 = (1 / 2) * mb2 * rb2**2
 Ib3 = (2 / 5) * mb3 * rb3**2
 Ib4 = (1 / 2) * mb4 * rb4**2
 Ib5 = (2 / 5) * mb5 * rb5**2
-Ib6 = mb6 / 4 * (rb6**2 + (hb6**2 / 3))
+Ib6 = (1 / 12) * mb6 * (bb6**2 + cb6**2)
 #Abstände zur Drehachse
 b1 = ufloat(0.0125, 0.003)
 b2 = ufloat(0.016, 0.003)
@@ -182,9 +202,9 @@ Ib4 = Ib4 + mb4 * b4**2
 Ib5 = Ib5 + mb5 * b5**2
 Ib6 = Ib6 + mb6 * b6**2
 
-Ik = Ik1 + Ik2 + Ik3 + 2 * (It1 + It2 + It3 + Ib1 + Ib2 + Ib3 + Ib4 + Ib5 + Ib6)
+Ik = Ik1 + Ik2 + Ik3 + It1 + It2 + It3 + 2 * (Ib1 + Ib2 + Ib3 + Ib4 + Ib5 + Ib6)
 
-#Trägheit Arme (a = ausgestreckt) (s = Steiner)
+#Trägheit Arme (a = ausgestreckt)
 Ia1 = (2 / 5) * ma1 * ra1**2
 Ia2 = (1 / 2) * ma2 * ra2**2
 Ia2a = ma2 / 4 * (ra2**2 + (ha2**2 / 3))
@@ -227,4 +247,30 @@ Ia6 = Ia6 + ma6 * a6**2
 Ia = Ik + 2 * (Ia1 + Ia2 + Ia3 + Ia4 + Ia5 + Ia6)
 Ic = Ik + 2 * (Ic1 + Ic2 + Ic3 + Ic4 + Ic5 + Ic6)
 
-print(Ia, Ic)
+#print(Ia, Ic)
+
+#Zeitmessung
+
+t1 = ufloat(3.22, 0.5)
+t2 = ufloat(3.16, 0.5)
+t3 = ufloat(3.22, 0.5)
+t4 = ufloat(3.08, 0.5)
+t5 = ufloat(3.17, 0.5)
+t6 = ufloat(4.33, 0.5)
+t7 = ufloat(4.26, 0.5)
+t8 = ufloat(4.20, 0.5)
+t9 = ufloat(4.57, 0.5)
+t10 = ufloat(4.51, 0.5)
+
+x = np.array([t1, t2, t3, t4, t5])
+y = np.array([t6, t7, t8, t9, t10])
+
+T1 = np.mean(x)
+T2 = np.mean(y)
+
+D = ufloat(23.34, 1.43) * 1e-3
+
+I1 = (T1**2 * D) / (4 * np.pi**2)
+I2 = (T2**2 * D) / (4 * np.pi**2)
+
+#print(I1, I2)
