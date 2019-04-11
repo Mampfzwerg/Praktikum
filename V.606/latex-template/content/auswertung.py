9e3749a4e4c6bc807bc89da2d12f33bfc8a54b3f
Sara Krieg <sara.krieg@tu-dortmund.de>
2019-04-11 21:42:21 +0200

    Auswertung

--- V.606/latex-template/content/auswertung.py
+++ V.606/latex-template/content/auswertung.py
@@ -13,11 +13,26 @@ Q = m/(l*p)
 
 U0, R0, Um, Rm = np.genfromtxt('mess3.txt', unpack=True)
 
-F = 5
-
+F = 86.6*10**-6
+#
 dR = Rm-R0
-print(2*((dR*F)/(Rm*Q)))
-print(2*((dR*F)/(R0*Q)))
+R3 = 998
+dU = U0-Um
+#
+x = 2*(dR/R3)*(F/(Q[3]*0.0001))
+
+print(x)
+#print(2*((dR*F)/(R0*Q[2])))
 
 #gemittelte Werte
+
+a = np.array([0.0020,0.0024,0.0025])
+b = np.array([0.0045,0.0025,0.0016])
+c = np.array([0.0032,0.0053,0.0022])
+d = np.array([0.0031,0.0044,0.0049])
+
+print(np.mean(a))
+print(np.mean(b))
+print(np.mean(c))
+print(np.mean(d))
 
--- V.606/latex-template/content/plot1.py
+++ V.606/latex-template/content/plot1.py
@@ -1,13 +1,49 @@
 import numpy as np
+from scipy.stats import sem
+import scipy.constants as const
+from scipy import optimize
 from uncertainties import ufloat
+import uncertainties.unumpy as unp
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
-U, f = np.genfromtxt('mess1.txt', unpack=True)
+U,f = np.genfromtxt('mess1.txt', unpack=True)
+
+def h (x, x0, y, a):
+    return a/((x**2-x0**2)**2+y**2*x0**2)
+
+par, cov = optimize.curve_fit(h, U[5:], f[5:])
+x_plot = np.linspace(19.7, 40.3, 1000)
+plt.plot(x_plot, h(x_plot, *par), 'b-', label=r'Ausgleichskurve', linewidth=1)
 
 plt.plot(U, f, 'rx', label='Messdaten')
 plt.xlabel(r'$\nu \: / \: $kHz')
 plt.ylabel(r'$U_A \: / \:$ V')
-plt.legend(loc='best')
 
+x0 = ufloat(par[0], np.sqrt(cov[0][0]))
+y = ufloat(par[1], np.sqrt(cov[1][1]))
+a = ufloat(par[2], np.sqrt(cov[2][2]))
+nu_0 = x0
+
+print('Parameter Gamma: ', y)
+print('Parameter a: ', a)
+print("Maximalwert: ", nu_0)
+
+nu_p = unp.sqrt(unp.sqrt(unp.sqrt(2)-1)*x0*y + x0**2)
+nu_m = unp.sqrt(-unp.sqrt(unp.sqrt(2)-1)*x0*y + x0**2)
+
+print("Neg. Frequnez: ", nu_p)
+print("Pos: Frequenz:", nu_m)
+
+# Güte:
+Q = nu_0/(nu_p-nu_m)
+print("Güte:",Q)
+
+#Gerade 
+h = a/(y**2 * x0**2)/np.sqrt(2)
+plt.plot(x_plot, 0*x_plot + h.nominal_value, 'g-',label= r'$\frac{1}{\sqrt{2}}$', linewidth=1.3)
+
+
+plt.legend(loc='best')
 plt.tight_layout()
 plt.savefig('plot1.pdf')
